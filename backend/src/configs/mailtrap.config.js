73233b48281479ce83e8d8f1e045c3c223afc45c
Razi9913 <skraz9913@gmail.com>
2024-09-13 10:20:07 +0530

    mailtaap config comment

--- backend/src/configs/mailtrap.config.js
+++ backend/src/configs/mailtrap.config.js
@@ -39,7 +39,7 @@ export const sender = {
 
 // import { MailtrapClient } from "mailtrap";
 
-// const TOKEN = "512a8dc3aef3f481ce04f29157266ee9";
+// const TOKEN = "exampleToken";
 
 // const client = new MailtrapClient({ token: TOKEN });
 
